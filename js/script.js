97c6ea2799a56669ee1b4331075734400d146647
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2017-09-28 21:36:01 +0200

    few small impovements

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
+(function() {
+    var a = document.querySelector('#aValue')
+    var h = document.querySelector('#hValue')
+    var result = document.querySelector('#result')
 
-// Triangle Area - JS
+    a.addEventListener('input', function() {
+        render(getTriangleArea(a.value, h.value));
+    })
 
-var triangle1Area = calculate(15,6);
-var triangle2Area = calculate(10,1);
-var triangle3Area = calculate(1,1);
+    h.addEventListener('input', function() {
+        render(getTriangleArea(a.value, h.value));
+    })
 
-function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0) {
-		alert('Nieprawidłowe dane');
-	} else {
-		showResult(a, h);
-	}
-}
+    function render(value) {
+        result.value = value || 0;
+    }
 
-function showResult(a, h) {
-	document.getElementById('result').value = calculate(a, h);
-}
+    function getTriangleArea(a, h) {
+        if (a < 0 || h < 0) {
+            alert('Nieprawidłowe dane');
+        } else {
+            return calculate(a, h);
+        }
+    }
 
-function calculate(a, h) {
-	return (a * h) / 2;
-}
+    function calculate(a, h) {
+        return (a * h) / 2;
+    }
 
-
-console.log(calculate(10, 2));
+    render();
+}())
